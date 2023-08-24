0e1fa8d759d326103d85dd676304a6c3082d3f62
Elvis Hodzic <eli.hodzic7@gmail.com>
2023-08-24 19:07:58 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <section className="flex-start flex-col paddings mb-16">
+            <h1>Kategorije</h1>
+            <h1>Objave</h1>
+            <h1>Učitaj više</h1>
+        </section>
+    )
+}
+
+export default Home
